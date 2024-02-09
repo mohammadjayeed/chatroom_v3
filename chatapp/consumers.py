5e273e38ccdcdee9131b627d1562e7cd6b3ca16e
Mohammad Jayeed <thisisjayeed@gmail.com>
2024-02-09 18:01:46 +0600

    chat feature complete

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,6 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
+from .models import ChatRoom, ChatMessage
+from django.contrib.auth.models import User
 
 class ChatConsumer(AsyncWebsocketConsumer):
     
@@ -21,3 +23,46 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
 
         )
+    
+    async def receive(self,text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+
+            self.room_group_name,{
+
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+
+        )
+        if message:
+            await self.save_message(username,room,message)
+    async def chat_message(self,event):
+
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps(
+        {
+            'message': message,
+            'username':username,
+            'room':room
+
+        }
+
+
+        ))
+    
+    @sync_to_async
+    def save_message(self,username,room,message):
+        user = User.objects.get(username=username)
+        room = ChatRoom.objects.get(slug=room)
+
+        ChatMessage.objects.create(user=user, room=room, message_content= message)
